3549632238d2ff115288fee6f20b47f5efb80e02
Trnaanjani <156882894+Trnaanjani@users.noreply.github.com>
2024-08-06 01:36:06 -0700

    Modul 1 (1) Modul 2 (1,2,3) Modul 3 (1)

---
+++ Modul_1/src/modul_1/Modul_1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul_1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Modul_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*     Tugas1 coba=new  Tugas1();
+        coba.setVisible(true);*/
+
+        /*Modul2_1 modul= new Modul2_1();
+        modul.setVisible(true);*/
+        
+        /*Modul2_2 modul= new Modul2_2();
+        modul.setVisible(true);*/
+        
+        Modul2_3 modul= new Modul2_3();
+        modul.setVisible(true);
+    }
+
+}
