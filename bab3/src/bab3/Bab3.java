0194d6443c2a16d3e56db1c06ea3249759a799e5
Triana Anjani <156882894+Trnaanjani@users.noreply.github.com>
2024-08-19 23:05:25 -0700

    Modul 3, 4, 6 dan 6

---
+++ bab3/src/bab3/Bab3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    /*public static void main(String[] args) {
+        tBab3 coba=new tBab3();
+        coba.setVisible(true);
+    }*/
+    
+    /*public static void main(String[] args) {
+        tBab4_1 coba=new tBab4_1();
+        coba.setVisible(true);*/
+    
+    /*public static void main(String[] args) {
+        tBab5 coba=new tBab5();
+        coba.setVisible(true);*/
+    
+    public static void main(String[] args) {
+        tBab6 coba=new tBab6();
+        coba.setVisible(true);
+    }
+
+    
+}
