92dc3d9c03534af0b1d94fc3b0420991a9091001
Triana Anjani <156882894+Trnaanjani@users.noreply.github.com>
2024-09-03 11:11:25 +0700

    Modul 8

---
+++ Modul8/src/main/java/com/mycompany/modul8/Modul8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modul8;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Modul8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
